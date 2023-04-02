6e6759ec41bb7167cd49d43e0c3ee9a54d82b93b
Paulo Endoh <paulo.endoh@gmail.com>
2023-04-02 14:29:20 -0300

    ux

--- src/components/_common/image/MyNextImage300/MyNextImage300.tsx
+++ src/components/_common/image/MyNextImage300/MyNextImage300.tsx
@@ -5,7 +5,20 @@ type Props = React.ComponentProps<typeof Image>
 
 const MyNextImage300 = React.forwardRef<HTMLImageElement, Props>(
   (props, ref) => {
-    return <Image ref={ref} {...props} width={300} height={300} />
+    return (
+      <Image
+        ref={ref}
+        {...props}
+        style={{
+          width: props.width,
+          height: props.height,
+
+          ...props.style,
+        }}
+        width={300}
+        height={300}
+      />
+    )
   }
 )
 
