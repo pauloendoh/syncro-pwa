602d4d8ccea8d6c242b0cf2ba4d6ecf6b1bbd0ee
Paulo Endoh <paulo.endoh@gmail.com>
2023-04-25 14:45:09 -0300

    Manga chapters and status [clcieys9i161150m01lkcp3p4f]

--- src/hooks/react-query/syncro-item/useEditItemMutation.ts
+++ src/hooks/react-query/syncro-item/useEditItemMutation.ts
@@ -1,19 +1,19 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { upsert } from 'endoh-utils'
 import { SyncroItemDto } from '../../../types/domain/syncro-item/SyncroItemDto'
 import { myNotifications } from '../../../utils/mantine/myNotifications'
 import { urls } from '../../../utils/urls'
 import { useAxios } from '../../../utils/useAxios'
-import { ItemRecommendationDto } from '../notification/types/ItemRecommendationDto'
 
 const useEditItemMutation = () => {
   const axios = useAxios()
   const qc = useQueryClient()
   return useMutation(
-    (payload: SyncroItemDto) =>
-      axios
-        .put<SyncroItemDto>(urls.api.syncroItem, payload)
-        .then((res) => res.data),
+    (payload: SyncroItemDto) => {
+      const { mangaExtraInfo, ...cleanPayload } = payload
+      return axios
+        .put<SyncroItemDto>(urls.api.syncroItem, cleanPayload)
+        .then((res) => res.data)
+    },
     {
       onSuccess: (saved) => {
         myNotifications.success('Item saved!')
