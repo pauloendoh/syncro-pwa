f4b13a6068b93b62fd2852ad0bacdb4df8bb4495
Paulo Endoh <paulo.endoh@gmail.com>
2023-04-25 16:11:56 -0300

    Manga chapters and status [clcieys9i161150m01lkcp3p4f]

--- src/components/SyncroItemPage/MangaExtraInfoSection/MangaExtraInfoSection.tsx
+++ src/components/SyncroItemPage/MangaExtraInfoSection/MangaExtraInfoSection.tsx
@@ -9,8 +9,10 @@ type Props = {
 const MangaExtraInfoSection = (props: Props) => {
   return (
     <FlexCol>
-      {props.info.chaptersQty > 0 && (
+      {props.info.chaptersQty > 0 ? (
         <Text>Chapters: {props.info.chaptersQty}</Text>
+      ) : (
+        <Text>Chapters: Unknown</Text>
       )}
       {props.info.status.length > 0 && <Text>Status: {props.info.status}</Text>}
     </FlexCol>
