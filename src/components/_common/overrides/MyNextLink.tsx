ace817c9f34f3537932ebc9c1a587d18e5aef855
Paulo Endoh <paulo.endoh@gmail.com>
2023-05-04 20:41:29 -0300

    ux - link progress

--- src/components/_common/overrides/MyNextLink.tsx
+++ src/components/_common/overrides/MyNextLink.tsx
@@ -1,10 +1,14 @@
+import { nprogress } from '@mantine/nprogress'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import React from 'react'
 
 type Props = React.ComponentProps<typeof Link>
 
 // override link with ref
 const MyNextLink = React.forwardRef<HTMLAnchorElement, Props>((props, ref) => {
+  const router = useRouter()
+
   return (
     <Link
       {...props}
@@ -15,6 +19,15 @@ const MyNextLink = React.forwardRef<HTMLAnchorElement, Props>((props, ref) => {
         width: 'fit-content',
 
         ...props.style,
+      }}
+      onClick={(e) => {
+        if (router.asPath !== props.href) {
+          nprogress.start()
+        }
+
+        if (props.onClick) {
+          props.onClick(e)
+        }
       }}
     >
       {props.children}
