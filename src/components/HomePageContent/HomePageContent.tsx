55699e3b685180aeb1bc262dace120f4fb3207d3
Paulo Endoh <paulo.endoh@gmail.com>
2023-06-01 09:28:51 -0300

    Can visualize other users' planned items [clffq1lf700050b0kr962xr44]

--- src/components/HomePageContent/HomePageContent.tsx
+++ src/components/HomePageContent/HomePageContent.tsx
@@ -56,43 +56,43 @@ const HomePageContent = () => {
       )}
       <Container fluid>
         <Grid>
-          <Grid.Col span={0} xs={0} sm={0} md={1} lg={2} xl={4} />
-          <Grid.Col span={12} xs={12} sm={7} md={7} lg={5} xl={4}>
-            <Container
-              size="xs"
-              fluid={isSmallScreen}
-              px={isSmallScreen ? 0 : undefined}
-              pt={isSmallScreen ? 24 : undefined}
-            >
-              <FlexVCenter justify={'space-between'}>
-                <Title order={4}>Feed</Title>
-                <ActionIcon onClick={openFeedSettingsModal}>
-                  <MdSettings />
-                </ActionIcon>
-              </FlexVCenter>
+          {/* <Grid.Col span={0} xs={0} sm={0} md={1} lg={2} xl={4} />
+          <Grid.Col span={12} xs={12} sm={7} md={7} lg={5} xl={4}> */}
+          <Container
+            size="xs"
+            fluid={isSmallScreen}
+            px={isSmallScreen ? 0 : undefined}
+            pt={isSmallScreen ? 24 : undefined}
+          >
+            <FlexVCenter justify={'space-between'}>
+              <Title order={4}>Feed</Title>
+              <ActionIcon onClick={openFeedSettingsModal}>
+                <MdSettings />
+              </ActionIcon>
+            </FlexVCenter>
 
-              <RatingsTimeline />
+            <RatingsTimeline />
 
-              {!isLoading && flatRatings.length === 0 && (
-                <Box sx={{ height: 400 }}>
-                  <Text>
-                    Your feed is empty. Check some
-                    <MyNextLink
-                      href={urls.pages.explore('popular-users')}
-                      style={{
-                        color: theme.colors.primary[9],
-                      }}
-                    >
-                      {' '}
-                      users{' '}
-                    </MyNextLink>
-                    to follow.
-                  </Text>
-                </Box>
-              )}
-            </Container>
-          </Grid.Col>
-          <Grid.Col span={0} xs={0} sm={5} md={4} lg={4} xl={4}></Grid.Col>
+            {!isLoading && flatRatings.length === 0 && (
+              <Box sx={{ height: 400 }}>
+                <Text>
+                  Your feed is empty. Check some
+                  <MyNextLink
+                    href={urls.pages.explore('popular-users')}
+                    style={{
+                      color: theme.colors.primary[9],
+                    }}
+                  >
+                    {' '}
+                    users{' '}
+                  </MyNextLink>
+                  to follow.
+                </Text>
+              </Box>
+            )}
+          </Container>
+          {/* </Grid.Col>
+          <Grid.Col span={0} xs={0} sm={5} md={4} lg={4} xl={4}></Grid.Col> */}
         </Grid>
       </Container>
     </LoggedLayout>
