194cd5c34cc153f8ac2befce5f2fecadcdf09f05
Paulo Endoh <paulo.endoh@gmail.com>
2023-07-13 14:41:53 -0300

    Can't close modal when I come from Twitter link [clk0erg7o001nxj0k9l6bt780]

--- src/utils/router/routerBackIfSameDomain.ts
+++ src/utils/router/routerBackIfSameDomain.ts
@@ -5,8 +5,16 @@ export const routerBackIfSameDomainOrClearQueryParam = (queryParam: string) => {
   const previousPath = sessionStorage.getItem(sessionStorageKeys.previousPath)
   const currentPath = Router.asPath
 
+  const cameFromSomewhere = !!document.referrer
+
+  console.log({
+    previousPath,
+    currentPath,
+    cameFromSomewhere,
+  })
+
   if (previousPath) {
-    if (previousPath !== currentPath) {
+    if (previousPath !== currentPath && cameFromSomewhere) {
       Router.back()
       return
     }
