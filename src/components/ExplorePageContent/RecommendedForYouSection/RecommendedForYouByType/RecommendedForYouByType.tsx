5a4ccf33273c97428eb3ad78c987b97cf7abe5fb
Paulo Endoh <paulo.endoh@gmail.com>
2023-07-17 14:16:51 -0300

    Recommended items for you [clfo2a4h8000svw0kfw33td5i]

--- src/components/ExplorePageContent/RecommendedForYouSection/RecommendedForYouByType/RecommendedForYouByType.tsx
+++ src/components/ExplorePageContent/RecommendedForYouSection/RecommendedForYouByType/RecommendedForYouByType.tsx
@@ -4,6 +4,8 @@ import { useItemRecommendationsForMeQuery } from '../../../../hooks/react-query/
 import { SyncroItemType } from '../../../../types/domain/syncro-item/SyncroItemType/SyncroItemType'
 import FavoriteItem from '../../../UserProfilePage/FavoritesSection/FavoritesByType/FavoritesByType/FavoriteItem/FavoriteItem'
 import FlexCol from '../../../_common/flex/FlexCol'
+import CenterLoader from '../../../_common/overrides/CenterLoader/CenterLoader'
+import Span from '../../../_common/text/Span'
 
 type Props = {
   type: SyncroItemType
@@ -14,16 +16,18 @@ const RecommendedForYouByType = ({ ...props }: Props) => {
     itemType: props.type,
   })
 
-  const { data } = useItemRecommendationsForMeQuery(props.type)
+  const { data, isLoading } = useItemRecommendationsForMeQuery(props.type)
 
   return (
     <FlexCol className="RecommendedForYouByType" gap={16}>
       <Title order={5}>{typeMap.getTypeLabel()}</Title>
       <ScrollArea pb={8}>
         <Flex gap={8}>
+          {isLoading && <CenterLoader height={133} />}
           {data?.map((item) => (
             <FavoriteItem key={item.id} item={item} alwaysShowTitle />
           ))}
+          {data?.length === 0 && <Span>No recommendations for you yet</Span>}
         </Flex>
       </ScrollArea>
     </FlexCol>
