2c377c0e2fcedf9d45a6661b79d21d9f87819e1e
Paulo Endoh <paulo.endoh@gmail.com>
2023-07-20 20:50:27 -0300

    ux

--- src/components/HomePage/RatingsTimeline/RatingsTimeline.tsx
+++ src/components/HomePage/RatingsTimeline/RatingsTimeline.tsx
@@ -1,6 +1,7 @@
-import { Center, Loader } from '@mantine/core'
+import { Box, Center, Loader } from '@mantine/core'
 import { useIntersection } from '@mantine/hooks'
 import { useEffect, useMemo, useRef } from 'react'
+import { Virtuoso } from 'react-virtuoso'
 import { useTimelineRatingsQuery } from '../../../hooks/react-query/feed/useHomeRatingsQuery'
 import { useTimelineHasNewsQuery } from '../../../hooks/react-query/feed/useTimelineHasNewsQuery'
 import { useMyMediaQuery } from '../../../hooks/useMyMediaQuery'
@@ -20,7 +21,7 @@ const RatingsTimeline = (props: Props) => {
     isLoading,
   } = useTimelineRatingsQuery(props.userId)
 
-  const containerRef = useRef()
+  const containerRef = useRef<HTMLDivElement>(null)
 
   const { ref, entry } = useIntersection({
     root: containerRef.current,
@@ -45,16 +46,26 @@ const RatingsTimeline = (props: Props) => {
     <>
       {isLoading && <CenterLoader />}
 
-      <FlexCol gap={16} mt={16}>
-        {flatRatings.map((rating) => (
-          <HomeRatingItem rating={rating} key={rating.id} />
-        ))}
-
-        {hasNextPage && (
-          <Center sx={{ height: 80 }} ref={ref}>
-            <Loader />
-          </Center>
-        )}
+      <FlexCol gap={16} mt={16} ref={containerRef}>
+        <Virtuoso
+          totalCount={flatRatings.length}
+          itemContent={(index) => (
+            <Box mb={16}>
+              <HomeRatingItem rating={flatRatings[index]} />
+            </Box>
+          )}
+          useWindowScroll
+          components={{
+            Footer: () => {
+              if (!hasNextPage) return null
+              return (
+                <Center sx={{ height: 80 }} ref={ref}>
+                  <Loader />
+                </Center>
+              )
+            },
+          }}
+        />
       </FlexCol>
     </>
   )
