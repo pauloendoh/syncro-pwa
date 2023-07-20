b85f04cb1f36aee67e20baa64ae6407fa47586c9
Paulo Endoh <paulo.endoh@gmail.com>
2023-07-20 20:21:26 -0300

    ux

--- src/utils/router/routerBackIfSameDomain.ts
+++ src/utils/router/routerBackIfSameDomain.ts
@@ -1,5 +1,4 @@
 import Router from 'next/router'
-import { sessionKeys } from '../sessionStorageKeys'
 
 /**
  * This solution is still not perfect.
@@ -7,21 +6,10 @@ import { sessionKeys } from '../sessionStorageKeys'
  * I want to replicate https://twitter.com/compose/tweet
  */
 export const routerBackIfSameDomainOrClearQueryParam = (queryParam: string) => {
-  const previousPath = sessionStorage.getItem(sessionKeys.previousPath)
-  const intermediaryPath = Router.asPath
-
-  if (previousPath) {
-    if (previousPath !== intermediaryPath) {
-      Router.back()
-
-      // if path didn't change, that means we are currently in the first URL of window.history
-      if (Router.asPath !== intermediaryPath) {
-        return
-      }
-    }
+  const closeRedirect = () => {
+    delete Router.query[queryParam]
+    Router.replace(Router, undefined, { scroll: false })
   }
 
-  delete Router.query[queryParam]
-  Router.push(Router, undefined, { scroll: false })
-  return
+  closeRedirect()
 }
