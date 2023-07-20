75a7a22875601ce86ffe7bfd2f1bdb0208a68761
Paulo Endoh <paulo.endoh@gmail.com>
2023-07-20 20:47:52 -0300

    ux

--- src/components/HomePage/RatingsTimeline/RatingsTimeline.tsx
+++ src/components/HomePage/RatingsTimeline/RatingsTimeline.tsx
@@ -1,7 +1,6 @@
-import { Box, Center, Loader } from '@mantine/core'
+import { Center, Loader } from '@mantine/core'
 import { useIntersection } from '@mantine/hooks'
 import { useEffect, useMemo, useRef } from 'react'
-import { Virtuoso } from 'react-virtuoso'
 import { useTimelineRatingsQuery } from '../../../hooks/react-query/feed/useHomeRatingsQuery'
 import { useTimelineHasNewsQuery } from '../../../hooks/react-query/feed/useTimelineHasNewsQuery'
 import { useMyMediaQuery } from '../../../hooks/useMyMediaQuery'
@@ -47,19 +46,9 @@ const RatingsTimeline = (props: Props) => {
       {isLoading && <CenterLoader />}
 
       <FlexCol gap={16} mt={16}>
-        <Virtuoso
-          totalCount={flatRatings.length}
-          itemContent={(index) => (
-            <Box mb={16}>
-              <HomeRatingItem rating={flatRatings[index]} />
-            </Box>
-          )}
-          useWindowScroll
-        />
-
-        {/* {flatRatings.map((rating) => (
+        {flatRatings.map((rating) => (
           <HomeRatingItem rating={rating} key={rating.id} />
-        ))} */}
+        ))}
 
         {hasNextPage && (
           <Center sx={{ height: 80 }} ref={ref}>
