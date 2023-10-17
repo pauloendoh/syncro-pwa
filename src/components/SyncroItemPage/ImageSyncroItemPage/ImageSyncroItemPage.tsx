35c1175102d726fa0e409814178b4911fef54d2e
Paulo Endoh <paulo.endoh@gmail.com>
2023-10-17 13:10:10 -0300

    item preview -> image -> zoom image [clml6o6d70001y80k8mv4meuf]

--- src/components/SyncroItemPage/ImageSyncroItemPage/ImageSyncroItemPage.tsx
+++ src/components/SyncroItemPage/ImageSyncroItemPage/ImageSyncroItemPage.tsx
@@ -1,9 +1,7 @@
 import { PhotoSlider } from 'react-photo-view'
 import { useQueryParams } from '../../../hooks/useQueryParams'
 import { SyncroItemDto } from '../../../types/domain/syncro-item/SyncroItemDto'
-import { urls } from '../../../utils/urls/urls'
 import SyncroItemImage from '../../_common/image/SyncroItemImage/SyncroItemImage'
-import MyNextLink from '../../_common/overrides/MyNextLink'
 
 type Props = {
   isMobile: boolean
@@ -14,12 +12,12 @@ type Props = {
 const ImageSyncroItemPage = ({ isMobile, item, ...props }: Props) => {
   const { queryValue, setQuery, removeQuery } = useQueryParams().itemImageOpen
 
-  if (props.isPreview)
-    return (
-      <MyNextLink href={urls.pages.syncroItem(encodeURI(item.id!))}>
-        <SyncroItemImage item={item} width={120} />
-      </MyNextLink>
-    )
+  // if (props.isPreview)
+  //   return (
+  //     <MyNextLink href={urls.pages.syncroItem(encodeURI(item.id!))}>
+  //       <SyncroItemImage item={item} width={120} />
+  //     </MyNextLink>
+  //   )
 
   return (
     <>
