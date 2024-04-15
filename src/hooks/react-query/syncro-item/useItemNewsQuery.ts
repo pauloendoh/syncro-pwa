baa08dc9a9a262070107ba1e3bf9583b6ff8cf87
Paulo Endoh <paulo.endoh@gmail.com>
2024-04-15 20:43:30 -0300

    Mobile = ignore item news query, since it does not show [clv0z8zk5000ovg0lcegu42sk]

--- src/hooks/react-query/syncro-item/useItemNewsQuery.ts
+++ src/hooks/react-query/syncro-item/useItemNewsQuery.ts
@@ -1,11 +1,18 @@
 import { useQuery } from '@tanstack/react-query'
 import { AxiosError } from 'axios'
 import { urls } from '../../../utils/urls/urls'
+import { useAxios } from '../../../utils/useAxios'
 import { ItemNewsDto } from './types/ItemNewsDto'
 
 export const useItemNewsQuery = (syncroItemId: string) => {
+  const axios = useAxios(false)
+
   return useQuery<ItemNewsDto[], AxiosError>(
     [urls.api.itemNews(syncroItemId)],
+    () =>
+      axios
+        .get<ItemNewsDto[]>(urls.api.itemNews(syncroItemId))
+        .then((res) => res.data),
     {
       refetchOnWindowFocus: false,
     }
