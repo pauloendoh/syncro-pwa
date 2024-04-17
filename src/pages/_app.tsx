2e4e821c89b64c7d020feb8a0ba5c56dc3ac09b7
Paulo Endoh <paulo.endoh@gmail.com>
2024-04-16 22:20:04 -0300

    iphone inputs shoud not zoom [clv0mk616000lvg0lxo10sgpq]

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -91,7 +91,10 @@ export default function App(props: AppProps) {
         <title>Syncro</title>
         {/* favicon */}
         <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
+        />
       </Head>
 
       <ColorSchemeProvider
